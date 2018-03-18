bbfe982522b5f84cbe52cab02aa1e152529d1d5c
Mandeep Wraich <mandeepw@nulogy.com>
2018-03-18 16:20:00 -0400

    Generate spec for toggling forecast

--- src/components/Temperature.test.jsx
+++ src/components/Temperature.test.jsx
@@ -3,7 +3,21 @@ import Temperature from "./Temperature";
 it('renders correctly', () => {
   const wrapper = render(
     <Temperature city="Toronto" temp={10} toggleForecast={
-        () => {}}/>
+        () => {}} />
     );
     expect(wrapper).toMatchSnapshot();
 });
+
+it('calls toggle function when clicked', () => {
+  const spy = sinon.spy();
+  const wrapper = mount(
+    <Temperature city="Toronto" temp={10} toggleForecast={spy} />
+    );
+
+  wrapper
+    .find("div")
+    .first()
+    .simulate("click");
+
+  expect(spy.calledOnce).toEqual(true)
+})
